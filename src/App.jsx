d35c302b80bf3553fcbe5cef656a94c1159690c0
Pehssa <luanenricopeca@gmail.com>
2026-08-27 01:14:36 -0300

    link to logo on home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 //import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Logo from './pages/logo/Logo'
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/logo" element={<Logo/>}/>
       </Routes>
     </>
